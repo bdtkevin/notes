cf1c0101bd9a1374e45514a11f697aae22776568
Kévin <bdtkevin@gmail.com>
2021-04-27 19:15:59 +0200

    Created basic routes api

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const app = require('express')();
+const express = require('express');
+const app = express();
 const { db, port } = require('./config');
 
 db.connect((err) => {
@@ -10,8 +10,62 @@ db.connect((err) => {
   console.log('connected as id ' + db.threadId);
 });
 
-app.get('/', (req, res) => {
-  res.send('hola!');
+app.use(express.json());
+
+app.get('/api/notes/:userId', (req, res) => {
+  const userId = parseInt(req.params.userId, 10);
+  db.query(
+    'SELECT id, title, note, date_created, date_modified FROM notes WHERE id_user = ?',
+    [userId],
+    (err, results) => {
+      if (!err) {
+        res.status(200).json(results);
+      } else {
+        res.sendStatus(500);
+      }
+    }
+  );
+});
+
+app.post('/api/notes/new', (req, res) => {
+  const { userId, title, note } = req.body;
+  db.query(
+    'INSERT INTO notes(id_user, title, note) VALUES (?, ?, ?)',
+    [userId, title, note],
+    (err) => {
+      if (!err) {
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(500);
+      }
+    }
+  );
+});
+
+app.put('/api/notes/update', (req, res) => {
+  const { id, title, note } = req.body;
+  db.query(
+    'UPDATE notes SET title = ?, note = ? WHERE id = ?',
+    [title, note, id],
+    (err) => {
+      if (!err) {
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(500);
+      }
+    }
+  );
+});
+
+app.delete('/api/notes/delete/:id', (req, res) => {
+  const id = req.params.id;
+  db.query('DELETE FROM notes WHERE id = ?', [id], (err) => {
+    if (!err) {
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(500);
+    }
+  });
 });
 
 app.listen(port, () => {
